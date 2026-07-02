4b33bfa77e3a2b73208130bbdb7514b0205d896d
Nandhini <nandhinimalishatti@gmail.com>
2026-07-02 15:02:24 +0530

    links modification

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://agri-rho-three.vercel.app/api',
 });
 
 // Add a request interceptor to include the auth token
